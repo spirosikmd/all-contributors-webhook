d173e1fabd2efe41017d562098574f328058737d
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2017-01-31 21:46:08 +0100

    chore(index): integrate now logs for better logging on running service

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require('./lib/server');
 const winston = require('winston');
 
+require('now-logs')(process.env.NOW_LOGS_KEY);
+
 server.listen(server.get('port'), () => {
   winston.log('info', `all-contributors-webhook is running on port ${server.get('port')}`);
 });
